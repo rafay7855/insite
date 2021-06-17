6149f2a6f038c86e5caf17b2295e8c405dc7be87
vivian <rafay7855@gmail.com>
2021-06-16 22:28:33 -0400

    Updated model construction, built survey endpoints

--- backend/app/api/surveys.py
+++ backend/app/api/surveys.py
@@ -1,23 +1,27 @@
 from flask import jsonify, request, url_for, current_app
 from .. import db
 from tinydb import Query
-#from ..models import Survey, Question
+from ..models import Survey, Question
 from . import api
 
 
 @api.route('/surveys/')
 def get_surveys():
-    ##TODO
+    ##TODO - Pagination?
     pass
 
 @api.route('/surveys/<identifier>')
 def get_survey(identifier: str):
-    Survey = Query()
-    survey = db['surveys'].search(Survey.id == identifier)
+    s_query = Query()
+    survey = db['surveys'].search(s_query.identifier == identifier)
     return survey
 
 @api.route('/surveys/', methods=['POST'])
 def new_survey():
-    ##TODO
-    pass
+    try:
+        survey = Survey(request.json)
+    except:
+        # TODO:  handle invalid request
+        pass
+    db['surveys'].insert(survey.to_json())
 
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,9 @@
 from typing import List
 
+# Defining the Data model
+# Used to validate fields and types for db insertion
+# These define the standard request format for the rest of the program
+
 class SurveyResponse:
     def __init__(self, survey_id: int, q_responses: List, datetime):
         self.survey_id = survey_id
@@ -29,34 +33,47 @@ class QuestionResponse:
         return json_question_response
 
 
+class Question:
+    def __init__(self, **kwargs):
+        self.survey_id = kwargs['survey_id']
+        self.number: int = kwargs['number']
+        # Generate question identifier
+        self.question_id = f"{self.survey_id}-{self.number}"
+        self.response_type: str = kwargs['response_type']
+        # Questions may have alt text
+        self.text: str = kwargs['text']
+        if 'alt_text' in kwargs.keys():
+            self.alt_text: str = kwargs['alt_text']
+        else:
+            self.alt_text: str = ''
+    
+    def to_json(self):
+        json_question = {
+            'survey_id': self.survey_id,
+            'question_id': self.question_id,
+            'number': self.number,
+            'response_type' : self.response_type,
+            'text': self.text,
+        }
+        if hasattr(self, 'alt_text'):
+            json_question['alt_text'] = self.alt_text
+        return json_question
+
+
 class Survey:
-    def __init__(self, surveyid: int, category: str, questions: List):
-        self.id = surveyid
-        self.category = category
-        self.questions = questions
+    def __init__(self, **kwargs):
+        self.identifier: str = kwargs['identifier']
+        self.name: str = kwargs['name']
+        self.category: str = kwargs['category']
+        self.questions: List = [Question(q) for q in kwargs['questions']]
 
     def to_json(self):
         questions = [q.to_json() for q in self.questions]
         json_survey = {
-            'id': self.id,
+            'identifier': self.identifier,
+            'name': self.name,
             'category': self.category,
             'questions': questions
         }
         return json_survey
 
-
-class Question:
-    def __init__(self, number: int, text: str, response_type: str, alt_text=""):
-        self.number = number
-        self.response_type = response_type
-        self.text = text
-        self.alt_text = alt_text
-    
-    def to_json(self):
-        json_question = {
-            'number': self.number,
-            'response_type' : self.response_type,
-            'text': self.text,
-            'alt_text': self.alt_text
-        }
-        return json_question
