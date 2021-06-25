329269574ee3153a1204263f49127b3e4b28b0e4
vivian <rafay7855@gmail.com>
2021-06-25 16:49:20 -0400

    Added comments

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,8 +11,12 @@ def create_app(configname):
     app.config.from_object(configs[configname])
     configs[configname].init_app(app)
 
-    bootstrap.init_app(app)
+    bootstrap.init_app(app) 
+    # Using TinyDB until it becomes inadequate, then I will migrate to mongoDB
+    # Requires using Docker bind mount to persist data. 
+    # Will worry about that once I put this on my raspberry PI
     db_file = TinyDB('db.json')
+    # Set up tables
     db = {
         'surveys': db_file.table('surveys'),
         'responses': db_file.table('responses')
@@ -22,7 +26,7 @@ def create_app(configname):
     def hello():
         return '<h1>Hello World!</h1>'
 
-    @app.route('/user/<name>')
+    @app.route('/name/<name>')
     def user(name):
         return f'<h1>Hello {name}!</h1>'
 
