2a515e07b98e6018fd1478b2ec70b626e9253017
vivian <rafay7855@gmail.com>
2021-06-25 17:34:12 -0400

    Implemented API errors in responses

--- backend/app/api/responses.py
+++ backend/app/api/responses.py
@@ -2,16 +2,18 @@ from flask import jsonify, request, url_for, current_app
 from tinydb import Query
 from .. import db
 from ..models import SurveyResponse, QuestionResponse
-from . import api
-
+from . import api, errors
 
 @api.route('/surveys/<str:survey_id>/responses/')
 def get_survey_responses(survey_id):
     resp = Query()
     responses = db['responses'].search(resp.survey_id == survey_id)
-    return jsonify({
-        'survey_responses': [SurveyResponse(r).to_json() for r in responses]
-    })
+    if responses:
+        return jsonify({
+            'survey_responses': [SurveyResponse(r).to_json() for r in responses]
+        })
+    else:
+        errors.not_found()
 
 
 @api.route('/surveys/<str:survey_id>/responses', methods=['POST'])
@@ -19,7 +21,6 @@ def new_survey_response(survey_id):
     try:
         survey_response = SurveyResponse(**request.get_json())
     except:
-        # TODO error handling
-        pass
+        errors.bad_request() # returns
     db['responses'].insert(survey_response.to_json())
     return jsonify(survey_response.to_json()), 201
