db31dc39d7443b0518be06053707d076056dcfd0
vivian <rafay7855@gmail.com>
2021-07-01 09:05:30 -0400

    Updated models for better readability

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,88 +3,8 @@ import datetime as dt
 
 # Defining the Data model
 # Used to validate fields and types for db insertion
+# Used to generate dynamic fields
 # These define the standard request format for the rest of the program
-
-class SurveyResponse:
-    """Represents a response to a survey.
-
-    Attributes:
-        survey_id: a string identifying the survey this question belongs to
-        timestamp: an ISO formate datetime
-        q_responses: a list of QuestionResponses to some or all of the questions in the survey
-    """
-
-    def __init__(self, **kwargs):
-        self.survey_id = kwargs['survey_id']
-        self.timestamp = dt.datetime.now().isoformat()
-        self.q_responses = [QuestionResponse(**r) for r in kwargs['q_responses']]
-    
-    def to_json(self):
-        question_responses = [r.to_json() for r in self.q_responses]
-        json_survey_response = {
-            'survey_id': self.survey_id,
-            'q_responses': question_responses
-        }
-        return json_survey_response
-    
-
-class QuestionResponse:
-    """Represents a response to a question
-
-    Attributes:
-        question_id: a string representing the ID of the question
-        response: the response. type is the question's response type
-    """
-    def __init__(self, **kwargs):
-        self.question_id = kwargs['question_id']
-        self.response = kwargs['response']
-
-    def to_json(self):
-        json_question_response = {
-            'question_id': self.question_id,
-            'response': self.response
-        }
-        return json_question_response
-
-
-class Question:
-    """Represents a question in a survey.
-
-    Attributes:
-        survey_id: a string identifying the survey this question belongs to
-        number: and integer that denotes the number of this question.
-        question_id: a unique identifier for this question based on number and survey_id
-        response_type: a string denoting the type of response expected for this question
-        text: the question itself.
-        alt_text: optional alternative text
-    """ 
-    
-    def __init__(self, **kwargs):
-        self.survey_id: str = kwargs['survey_id']
-        self.number: int = kwargs['number']
-        # Generate question identifier
-        self.question_id: str = f"{self.survey_id}-{self.number}"
-        self.response_type: str = kwargs['response_type']
-        self.text: str = kwargs['text']
-        # Questions may have alt_text parameter
-        if 'alt_text' in kwargs.keys():
-            self.alt_text: str = kwargs['alt_text']
-        else:
-            self.alt_text: str = ''
-    
-    
-    def to_json(self):
-        # Return this object as a Json Dict
-        json_question = {
-            'survey_id': self.survey_id,
-            'question_id': self.question_id,
-            'number': self.number,
-            'response_type' : self.response_type,
-            'text': self.text,
-        }
-        if self.alt_text:
-            json_question['alt_text'] = self.alt_text
-        return json_question
 
 
 class Survey:
@@ -115,4 +35,86 @@ class Survey:
             'questions': [q.to_json() for q in self.questions]
         }
         return json_survey
+
+
+class Question:
+    """Represents a question in a survey.
+    Embedded inside a Survey
+
+    Attributes:
+        survey_id: a string identifying the survey this question belongs to
+        number: and integer that denotes the number of this question.
+        question_id: a unique identifier for this question based on number and survey_id
+        response_type: a string denoting the type of response expected for this question
+        text: the question itself.
+        alt_text: optional alternative text
+    """ 
+    
+    def __init__(self, **kwargs):
+        self.survey_id: str = kwargs['survey_id']
+        self.number: int = kwargs['number']
+        # Generate question identifier
+        self.question_id: str = f"{self.survey_id}-{self.number}"
+        self.response_type: str = kwargs['response_type']
+        self.text: str = kwargs['text']
+        # Questions may have alt_text parameter
+        if 'alt_text' in kwargs.keys():
+            self.alt_text: str = kwargs['alt_text']
+        else:
+            self.alt_text: str = ''
+    
+    
+    def to_json(self):
+        # Return this object as a Json Dict
+        json_question = {
+            'number': self.number,
+            'response_type' : self.response_type,
+            'text': self.text,
+        }
+        if self.alt_text:
+            json_question['alt_text'] = self.alt_text
+        return json_question
+
+
+class SurveyResponse:
+    """Represents a response to a survey.
+
+    Attributes:
+        survey_id: a string identifying the survey this question belongs to
+        timestamp: an ISO formate datetime
+        q_responses: a list of QuestionResponses to some or all of the questions in the survey
+    """
+
+    def __init__(self, **kwargs):
+        self.survey_id = kwargs['survey_id']
+        self.timestamp = dt.datetime.now().isoformat()
+        self.q_responses = [QuestionResponse(**r) for r in kwargs['q_responses']]
+    
+    def to_json(self):
+        question_responses = [r.to_json() for r in self.q_responses]
+        json_survey_response = {
+            'survey_id': self.survey_id,
+            'q_responses': question_responses
+        }
+        return json_survey_response
+    
+
+class QuestionResponse:
+    """Represents a response to a question.
+    Embedded inside a SurveyResponse
+
+    Attributes:
+        question_id: a string representing the ID of the question
+        response: the response. type is the question's response type
+    """
+    def __init__(self, **kwargs):
+        self.question_id = kwargs['question_id']
+        self.response = kwargs['response']
+
+    def to_json(self):
+        json_question_response = {
+            'question_id': self.question_id,
+            'response': self.response
+        }
+        return json_question_response
 
